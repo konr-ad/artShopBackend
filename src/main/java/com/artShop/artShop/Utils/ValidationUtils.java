a1d89aedc652f8852cd5e26c84630739ac2f5785
Konrad <konradk95@gmail.com>
2024-05-30 11:30:58 +0200

    Implementation of ValidationUtils class

---
+++ src/main/java/com/artShop/artShop/Utils/ValidationUtils.java
@@ -0,0 +1,25 @@
+package com.artShop.artShop.Utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class ValidationUtils {
+
+    public static ResponseEntity<?> getResponseEntity(BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+}
