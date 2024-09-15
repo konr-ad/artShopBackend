4bf4f1b9583ff2bf8ec64cd1d1206c51560c3f50
Konrad <konradk95@gmail.com>
2024-09-15 23:09:55 +0200

    Changes to cors policy v2

--- src/main/java/com/artShop/artShop/config/SecurityConfig.java
+++ src/main/java/com/artShop/artShop/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
--- src/main/java/com/artShop/artShop/config/WebConfig.java
+++ src/main/java/com/artShop/artShop/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.artShop.artShop.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,13 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.ip.address}")
+    private String frontendIP;
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Adjust this based on your API routes
-                .allowedOrigins("http://srv15.mikr.us:20158")  // Allow the frontend origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Methods allowed
-                .allowedHeaders("*")  // Allow all headers
-                .allowCredentials(true)  // Allow credentials (e.g., cookies)
-                .maxAge(3600);  // Cache pre-flight requests for 1 hour
+        logger.info("frontendIP: {}", frontendIP);
+        registry.addMapping("/api/**")
+                .allowedOrigins(frontendIP)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
