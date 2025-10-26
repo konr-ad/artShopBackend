74ff82f09a09a477d87538406cce2060fb756e47
Konrad <konradk95@gmail.com>
2025-10-26 11:27:10 +0100

    discountCodes stuff5

--- src/main/java/com/artshop/backend/api/dto/discount/DiscountCodeDto.java
+++ src/main/java/com/artshop/backend/api/dto/discount/DiscountCodeDto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record DiscountCodeDto(
+        Long id,
         String code,
         String discountType,
         BigDecimal discountValue,
